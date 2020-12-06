6e0e08444a6c31de3f67024b072d222d48912aa6
Vladlan <lanvlad911@gmail.com>
2020-12-07 02:45:56 +0300

    added cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   product: 'https://269pjntj1e.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://269pjntj1e.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://xherne2245.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://269pjntj1e.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://269pjntj1e.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://vladlan-cart-api-develop.eu-west-1.elasticbeanstalk.com/api'
 };
 
 export default API_PATHS;
